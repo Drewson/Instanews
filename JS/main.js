a89b3479cda43289f8c8b7a4f263ed173a2da7de
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-01-30 15:49:09 -0800

    Added hover state to the paragraphs, and transition to header items

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,34 @@
 $(function(){
-
-     var chosenCategory = $('#categories option:selected').text(); 
-
+    console.log("hello");
     $('#categories').change( '#categories option:selected', function(event){
         event.preventDefault();
         $('#newsFlex').empty();
 
         $('#loaderGif img').show();
 
-        chosenCategory = $('#categories option:selected').text().toLowerCase(); 
+        var chosenCategory = $('#categories option:selected').text().toLowerCase(); 
 
-        var url = 'https://api.nytimes.com/svc/topstories/v2/' + chosenCategory + '.json';
-        url += '?' + $.param({'api-key': 'da52f7a32bda4e16bac25f88e3162265'});
-
+         var url = 'https://api.nytimes.com/svc/topstories/v2/' + chosenCategory + '.json?api-key=da52f7a32bda4e16bac25f88e3162265'
         $.ajax({
             method : 'GET',
             url : url
         })
         .done(function(data){
-            var resultsArray = data.results
+            var articlesResults = data.results
             var listAppendage = '';
 
-            var filteredArray = resultsArray.filter(function(value){
+            var filteredArticles = articlesResults.filter(function(value){
                 return value.multimedia.length > 0;
             }).slice(0,12);
 
-            $.each(filteredArray, function(key, value){
+            $.each(filteredArticles, function(key, value){
                 var image = value.multimedia[4].url;
                 var articleText = value.abstract;
                 var articleLink = value.url;
             
-                listAppendage += '<li class="imageList">'; 
-                listAppendage += '<a href="' + articleLink + '"><div class="articleImg"'
-                listAppendage +=  'style="background-image:url(' + image + ')"></div>';
+                listAppendage += '<li>'; 
+                listAppendage += '<a href="' + articleLink + '">';
+                listAppendage += '<div class="articleImg" style="background-image:url(' + image + ')"></div>';
                 listAppendage += '<p>' + articleText + '</p>';
                 listAppendage += '</a></li>';
             })
